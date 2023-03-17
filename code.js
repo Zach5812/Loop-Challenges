5614af5697ecaa09bf38c5522230d37cd1b9f9ea
Zach5812 <zachlouk7@gmail.com>
2023-03-17 14:19:38 -0700

    first commit

---
+++ code.js
@@ -0,0 +1,25 @@
+// for(i=1; i<21; i++){
+//     console.log(i)
+// }
+
+// for(i=100; i>0; i--){
+//     if(i % 3 == 0)
+//     console.log(i)
+// }
+
+// var arr=["4","2.5","1","-.5","-2","-3.5"]
+// for(i=0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// var sum = 0;
+// for(i=1; i<101; i++){
+//     sum = sum + i;
+//     console.log(sum)
+// }
+
+// var product = 1;
+// for(i=1; i<13; i++){
+//     product = product * i;
+//     console.log(product)
+// }
